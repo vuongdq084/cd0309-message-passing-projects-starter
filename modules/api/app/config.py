7207e3fe47b0ebc1418e1284039640fecc6466c2
vuongdq084 <quocvuonga7sp@gmail.com>
2024-10-19 11:35:15 +0700

    modify for working in host pc development env

--- modules/api/app/config.py
+++ modules/api/app/config.py
@@ -1,11 +1,17 @@
 import os
 from typing import List, Type
 
-DB_USERNAME = os.environ["DB_USERNAME"]
-DB_PASSWORD = os.environ["DB_PASSWORD"]
-DB_HOST = os.environ["DB_HOST"]
-DB_PORT = os.environ["DB_PORT"]
-DB_NAME = os.environ["DB_NAME"]
+#DB_USERNAME = os.environ["DB_USERNAME"]
+#DB_PASSWORD = os.environ["DB_PASSWORD"]
+#DB_HOST = os.environ["DB_HOST"]
+#DB_PORT = os.environ["DB_PORT"]
+#DB_NAME = os.environ["DB_NAME"]
+
+DB_USERNAME = "ct_admin"
+DB_PASSWORD = "Abc12345"
+DB_HOST = "db"
+DB_PORT = "5432"
+DB_NAME = "geoconnections"
 
 
 class BaseConfig:
